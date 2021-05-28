f284259140c21a02ef5a6f06aa94317b9a3b4efc
HalfSummer11 <halfsummer11@gmail.com>
2021-05-28 23:35:06 +0800

    fix translation

--- train_aegan.py
+++ train_aegan.py
@@ -85,27 +85,75 @@ def random_choice_noreplace(l, n_sample, num_draw):
                              axis=-1)[:, :n_sample]]
 
 
-def ransac_fit_r(batch_dr, max_iter=100, thres=1.0, chosen_axis=None):
+def ransac_fit_r(batch_dr, max_iter=100, thres=5.0, chosen_axis=None, flip_axis=False):
     # B, 3, 3
     best_score = 0
     chosen_hyp = None
     nb = batch_dr.shape[0]
     if chosen_axis is not None:
         print('--- we are processing a symmetric object!!!')
+        batch_dr = batch_dr.transpose(-1, -2)
+
+    def proj_rot(rot, num):
+        cur_vec = rot[num]  # normalized
+        next_vec = rot[(num + 1) % 3]
+        proj = (next_vec * cur_vec).sum()
+        next_vec = next_vec - proj * cur_vec
+        next_vec = next_vec / torch.clamp(torch.sqrt((next_vec ** 2).sum()), min=1e-5)
+        final_vec = torch.cross(cur_vec, next_vec)
+        new_ret = torch.eye(3).to(rot.device)
+        new_ret[num] = cur_vec
+        new_ret[(num + 1) % 3] = next_vec
+        new_ret[(num + 2) % 3] = final_vec
+        return new_ret
+
+    def axis_mean(rot, chosen_axis, flip_axis):
+        char2num = {'x': 0, 'y': 1, 'z': 2}
+        num = char2num[chosen_axis]
+        axis = np.eye(3)[num]
+        axis = torch.tensor(axis).float().to(rot.device).reshape(1, 3, 1)
+        proj = torch.matmul(rot, axis)  # [B, 3, 3] * [B, 3, 1] -> [B, 3, 1]
+        if flip_axis:
+            proj_reverse = proj[:, 0:1]   # [B, 1, 1]
+            factor = torch.ones_like(proj_reverse)
+            factor[torch.where(proj_reverse < 0)[0]] = -1
+            proj = proj * factor
+        avg_axis = torch.mean(proj, dim=0)  # [3, 1]
+        avg_axis /= torch.norm(avg_axis, dim=0, keepdim=True)
+
+        ret = torch.eye(3).to(rot.device)
+        ret[num] = avg_axis.reshape(-1)
+        ret = proj_rot(ret, num)
+        ret = ret.transpose(-1, -2)
+        return ret
+
+    def compute_r(sample_idx):
+        r_samples = batch_dr[sample_idx]
+        if chosen_axis is not None:
+            r_hyp = axis_mean(r_samples, chosen_axis, flip_axis)
+            err = rot_diff_degree(r_hyp, batch_dr, chosen_axis=chosen_axis, flip_axis=flip_axis)
+        else:
+            r_hyp = so3_mean(r_samples.unsqueeze(0))
+            err = mean_angular_error(r_hyp, batch_dr) * 180 / np.pi
+        inliers = (err < thres) * 1.0
+        curr_score = inliers.mean()
+        return curr_score, r_hyp, torch.where(inliers)[0]
+
+    best_idx = random_choice_noreplace(torch.tensor(np.arange(nb)), 5, 1).squeeze()
     with torch.no_grad():
         for i in range(max_iter):
             sample_idx = random_choice_noreplace(torch.tensor(np.arange(nb)), 5, 1).squeeze()
-            r_samples = batch_dr[sample_idx]
-            r_hyp     = so3_mean(r_samples.unsqueeze(0))
-            if chosen_axis is not None:
-                err = rot_diff_degree(r_hyp, batch_dr, chosen_axis=chosen_axis)
-            else:
-                err = mean_angular_error(r_hyp, batch_dr) * 180 /np.pi
-            inliers = (err < thres) * 1.0
-            curr_score = inliers.mean()
+            curr_score, r_hyp, idx = compute_r(sample_idx)
             if curr_score > best_score:
                 best_score = curr_score
                 chosen_hyp = r_hyp
+        rec_score, rec_hyp, _ = compute_r(best_idx)
+        if rec_score > best_score:
+            best_score = rec_score
+            chosen_hyp = rec_hyp
+
+    if chosen_axis is not None:
+        chosen_hyp = chosen_hyp.transpose(-1, -2)
 
     return chosen_hyp, best_score
 
@@ -114,20 +162,32 @@ def ransac_fit_t(batch_dt, batch_dr, delta_r, max_iter=100, thres=0.025):
     best_score = 0
     chosen_hyp = None
     nb = batch_dt.shape[0]
-    dt_candidates = torch.matmul(-batch_dt, delta_r)
+    # dt_candidates = torch.matmul(-batch_dt, delta_r)
+
+    def compute_t(sample_idx):
+        t_samples = batch_dt[sample_idx]
+        t_hyp = t_samples.mean(dim=0, keepdim=True)
+        err = torch.norm(t_hyp, batch_dt, dim=-1)
+        inliers = (err < thres) * 1.0
+        curr_score = inliers.mean()
+        return curr_score, t_hyp, torch.where(inliers)[0]
+
+    best_idx = None
     with torch.no_grad():
         for i in range(max_iter):
             sample_idx = random_choice_noreplace(torch.tensor(np.arange(nb)), 5, 1).squeeze()
-            t_samples = dt_candidates[sample_idx]
-            t_hyp     = t_samples.mean(dim=0, keepdim=True)
-            err       = torch.norm(torch.matmul(t_hyp, delta_r.permute(1, 0).contiguous()) + batch_dt, dim=-1)
-            inliers   = (err < thres) * 1.0
-            curr_score = inliers.mean()
+            curr_score, t_hyp, idx = compute_t(sample_idx)
             if curr_score > best_score:
                 best_score = curr_score
                 chosen_hyp = t_hyp
+                best_idx = idx
+        rec_score, rec_hyp, _ = compute_t(best_idx)
+        if rec_score > best_score:
+            best_score = rec_score
+            chosen_hyp = rec_hyp
 
     return chosen_hyp, best_score
+
 
 @hydra.main(config_path="config/completion.yaml")
 def main(cfg):
@@ -244,27 +304,33 @@ def main(cfg):
         if cfg.pre_compute_delta:
             set_dr = []
             set_dt = []
+            pts = []
             sym_dict = infos.sym_type[cfg.target_category]
             chosen_axis = None
+            flip_axis = cfg.target_category in ['can']
             for key, M in sym_dict.items():
                 if M > 20:
                     chosen_axis = key
                     if 'modelnet' in cfg.name_dset:
                         chosen_axis = 'z'
             for num, data in enumerate(train_loader):
-                if num > 4096:
+                if num > 2048 / len(data): # 4096 / len(data): # 4096:
                     break
                 torch.cuda.empty_cache()
                 tr_agent.eval_func(data)
                 set_dr.append(tr_agent.pose_info['delta_r'])
-                if chosen_axis is not None:
-                    print(torch.matmul(tr_agent.pose_info['delta_r'], torch.Tensor([0, 0, 1]).view(1, 3, 1).contiguous().cuda()).squeeze())
+                # if chosen_axis is not None:
+                #     print(torch.matmul(tr_agent.pose_info['delta_r'], torch.Tensor([0, 0, 1]).view(1, 3, 1).contiguous().cuda()).squeeze())
                 if cfg.pred_t:
                     set_dt.append(tr_agent.pose_info['delta_t'])
+                pts.append(tr_agent.output_pts)
 
-            delta_r, r_score = ransac_fit_r(torch.cat(set_dr, dim=0), chosen_axis=chosen_axis)
+            delta_r, r_score = ransac_fit_r(torch.cat(set_dr, dim=0), chosen_axis=chosen_axis,
+                                            flip_axis=flip_axis)
             if cfg.pred_t:
                 delta_t, t_score = ransac_fit_t(torch.cat(set_dt, dim=0), torch.cat(set_dr, dim=0), delta_r.squeeze() )
+
+            pts = torch.cat(pts, dim=0).cpu().numpy()
 
             # valid_deltas = torch.cat(all_deltas, dim=0)
             # # valid_deltas = valid_deltas[valid_deltas[:, 0, 1]>0] # partial airplane
@@ -296,6 +362,9 @@ def main(cfg):
             save_name = f'{project_path}/haoi-pose/evaluation/infos/{cfg.exp_num}_{cfg.name_dset}_{cfg.target_category}.npy'
             print('saving to ', save_name)
             np.save(save_name, arr=save_dict)
+
+            np.savez_compressed(f'{project_path}/haoi-pose/evaluation/infos/{cfg.exp_num}_{cfg.name_dset}_{cfg.target_category}_pts.npz',
+                                pts=pts)
             return
 
         # main evaluation scripts
